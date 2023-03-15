510c7f9837aa1e6cb75aaff007dfc3cb8655b68c
Babasola Oso <easybabasola@gmail.com>
2023-03-15 11:55:56 +0100

    deploying to github pages 1

--- src/components/Work.js
+++ src/components/Work.js
@@ -15,31 +15,37 @@ const Work = () => {
               <div className='p-6'>
                 <h2 className='h2 leading-tight text-accent'>My Latest <br></br>Work</h2>
                 <p className='max-w-sm mb-16'>A WhatsApp-Clone Using Firebase !</p>
-                <button className='btn btn-sm'>View Project</button>
+                <a href='https://bbschatapp.netlify.app/'><button className='btn btn-sm'>View Project</button></a>
               </div>
 
-              <div className='group relative overflow-hidden border-2 border-white/50 rounded-xl'>
-                <div className='group-hover:bg-black/70 w-full h-full absolute z-40 transition-all duration-300'></div>
-                <img className='group-hover:scale-125 transition-all duration-500' src={Img1}></img>
-                <div className='absolute -bottom-full left-12 group-hover:bottom-24 transition-all duration-500 z-50'><span className='text-gradient'>Chat App</span></div>
-                <div className='absolute -bottom-full left-12 group-hover:bottom-14 transition-all duration-700 z-50'><span className='text-3xl text-white'>Project Title</span></div>
-              </div>
+              <a href='https://bbschatapp.netlify.app/'>
+                  <div className='group relative overflow-hidden border-2 border-white/50 rounded-xl'>
+                    <div className='group-hover:bg-black/70 w-full h-full absolute z-40 transition-all duration-300'></div>
+                    <img className='group-hover:scale-125 transition-all duration-500' src={Img1}></img>
+                    <div className='absolute -bottom-full left-12 group-hover:bottom-24 transition-all duration-500 z-50'><span className='text-gradient'>Chat App</span></div>
+                    <div className='absolute -bottom-full left-12 group-hover:bottom-14 transition-all duration-700 z-50'><span className='text-3xl text-white'>Project Title</span></div>
+                  </div>
+              </a>
             </motion.div>
 
             <motion.div variants={fadeIn('left', 0.2)} initial='hidden' whileInView={'show'} viewport={{once: false, amount: 0.3}} className='flex-1 flex flex-col gap-y-10'>
-                <div className='group relative overflow-hidden border-2 border-white/50 rounded-xl'>
-                  <div className='group-hover:bg-black/70 w-full h-full absolute z-40 transition-all duration-300'></div>
-                  <img className='group-hover:scale-125 transition-all duration-500' src={Img2}></img>
-                  <div className='absolute -bottom-full left-12 group-hover:bottom-24 transition-all duration-500 z-50'><span className='text-gradient'>Chat App</span></div>
-                  <div className='absolute -bottom-full left-12 group-hover:bottom-14 transition-all duration-700 z-50'><span className='text-3xl text-white'>Project Title</span></div>
-                </div>
-
-                <div className='group relative overflow-hidden border-2 border-white/50 rounded-xl'>
-                  <div className='group-hover:bg-black/70 w-full h-full absolute z-40 transition-all duration-300'></div>
-                  <img className='group-hover:scale-125 transition-all duration-500' src={Img3}></img>
-                  <div className='absolute -bottom-full left-12 group-hover:bottom-24 transition-all duration-500 z-50'><span className='text-gradient'>Chat App</span></div>
-                  <div className='absolute -bottom-full left-12 group-hover:bottom-14 transition-all duration-700 z-50'><span className='text-3xl text-white'>Project Title</span></div>
-                </div>
+                  <a href='https://bbschatapp.netlify.app/'>
+                    <div className='group relative overflow-hidden border-2 border-white/50 rounded-xl'>
+                      <div className='group-hover:bg-black/70 w-full h-full absolute z-40 transition-all duration-300'></div>
+                      <img className='group-hover:scale-125 transition-all duration-500' src={Img2}></img>
+                      <div className='absolute -bottom-full left-12 group-hover:bottom-24 transition-all duration-500 z-50'><span className='text-gradient'>Chat App</span></div>
+                      <div className='absolute -bottom-full left-12 group-hover:bottom-14 transition-all duration-700 z-50'><span className='text-3xl text-white'>Project Title</span></div>
+                    </div>
+                  </a>
+                  
+                  <a href='https://bbschatapp.netlify.app/'>
+                    <div className='group relative overflow-hidden border-2 border-white/50 rounded-xl'>
+                      <div className='group-hover:bg-black/70 w-full h-full absolute z-40 transition-all duration-300'></div>
+                      <img className='group-hover:scale-125 transition-all duration-500' src={Img3}></img>
+                      <div className='absolute -bottom-full left-12 group-hover:bottom-24 transition-all duration-500 z-50'><span className='text-gradient'>Chat App</span></div>
+                      <div className='absolute -bottom-full left-12 group-hover:bottom-14 transition-all duration-700 z-50'><span className='text-3xl text-white'>Project Title</span></div>
+                    </div>
+                  </a>
             </motion.div>
           </div>
         </div>
